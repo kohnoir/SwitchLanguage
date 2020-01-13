ab5b6f95c10cb17ff52a08b817118606f0dad414
serg0 <serg.01@bk.ru>
2020-01-13 10:36:42 +0300

    error rework

--- app/src/main/java/com/example/switchlanguage/MainActivity.java
+++ app/src/main/java/com/example/switchlanguage/MainActivity.java
@@ -1,55 +1,53 @@
 package com.example.switchlanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner sLang;
     Button btn;
-    Spinner sLang;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        initButtom();
+        initButton();
     }
     private void initViews(){
         sLang = findViewById(R.id.Spinner);
+        btn = findViewById(R.id.btn);
         initSpinner();
     }
     private void initSpinner(){
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Lang, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lang, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sLang.setAdapter(adapter);
     }
-    private void initButtom(){
-        Button btn = findViewById(R.id.btn);
+    private void initButton(){
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(sLang.getSelectedItem().toString().equals("eng")) {
-                    Locale locale = new Locale("eng");
-                    changeLocale(locale);
-                }else if(sLang.getSelectedItem().toString().equals("ru")){
-                    Locale locale = new Locale("ru");
-                    changeLocale(locale);
+                String englishItem = "eng";
+                String russianItem = "ru";
+                if (sLang.getSelectedItem().toString().equals(englishItem)) {
+                    changeLocale(englishItem);
+                } else if (sLang.getSelectedItem().toString().equals(russianItem)) {
+                    changeLocale(russianItem);
                 }
             }
         });
     }
     @SuppressWarnings("deprecation")
-    private void changeLocale(Locale locale) {
+    private void changeLocale(String language) {
+        Locale locale = new Locale(language);
         Locale.setDefault(locale);
         Configuration configuration = new Configuration();
         configuration.setLocale(locale);
@@ -61,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         setTitle(R.string.app_name);
 
         TextView txt = findViewById(R.id.textView);
-        txt.setText(R.string.Txt);
+        txt.setText(R.string.txt);
     }
 }
 
